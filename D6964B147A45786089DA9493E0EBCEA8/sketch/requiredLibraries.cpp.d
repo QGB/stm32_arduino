3ec9b80e3ccf32fbdbf49de7a75a0b8b4d22d25c
qgb <qgbcs1@gmail.com>
2024-07-14 22:35:03 +0800

    可直接烧录 stm32_arduino.ino.hex

---
+++ D6964B147A45786089DA9493E0EBCEA8/sketch/requiredLibraries.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\qgb\AppData\Local\Temp\arduino\sketches\D6964B147A45786089DA9493E0EBCEA8\sketch\requiredLibraries.cpp.o: \
+ C:\Users\qgb\AppData\Local\Temp\arduino\sketches\D6964B147A45786089DA9493E0EBCEA8\sketch\requiredLibraries.cpp \
+ C:\Users\qgb\AppData\Local\Arduino15\packages\STMicroelectronics\hardware\stm32\2.8.0\libraries\SrcWrapper\src/SrcWrapper.h
